e0de0af43794cdd08fe60f21557b98829cee66f7
Kien Doan <kdoan@MacBook-Pro-3.local>
2016-03-10 08:19:03 -0800

    first attempt at rock paper scissor

--- rock_paper_scissors.rb
+++ rock_paper_scissors.rb
@@ -1,14 +1,37 @@
 
-VALID_CHOICES = %w(rock paper scissors)
+VALID_CHOICES = %w(rock paper scissors spock lizard)
 
 def prompt(message)
   Kernel.puts "==> #{message}"
 end
 
 def win?(first, second)
-  (first == 'rock' && second == 'scissors')||
+  (first == 'rock' && second == 'scissors') ||
+  (first == 'rock' && second == 'lizard') ||
   (first == 'paper' && second == 'rock') ||
-  (first == 'scissors' && second == 'paper')
+  (first == 'paper' && second == 'spock') ||
+  (first == 'scissors' && second == 'paper')||
+  (first == 'scissors' && second == 'lizard')||
+  (first == 'lizard' && second == 'paper')||
+  (first == 'lizard' && second == 'spock')||
+  (first == 'spock' && second == 'rock')||
+  (first == 'spock' && second == 'scissors')
+end
+
+def short_cut(input)
+  word = case input
+    when '1'
+      'rock'
+    when '2'
+      'paper'
+    when '3'
+      'scissors'
+    when '4'
+      'spock'
+    when '5'
+      'lizard'
+    end
+  return word
 end
 
 def display_results(player, computer)
@@ -21,24 +44,48 @@ def display_results(player, computer)
   end
 end
 
+def keep_score(player, computer)
+  player = player.to_i
+  computer = computer.to_i
+  if win?(player, computer)
+    player += 1
+    prompt("#{player}")
+  elsif win?(computer, player)
+    computer += 1
+    prompt("#{computer}")
+  else
+    prompt("No points for both")
+  end
+end
+
 loop do
   choice = ''
   loop do
-    prompt("Choose one: #{VALID_CHOICES.join(', ')}")
+    prompt <<-MSG
+    Choose one using a number:
+    1) rock 
+    2) paper 
+    3) scissors 
+    4) spock 
+    5) lizard
+    MSG
     choice = Kernel.gets().chomp()
 
-    if VALID_CHOICES.include?(choice)
+    if %w(1 2 3 4 5).include?(choice)
       break
     else
       prompt("That's not a valid choice")
     end
   end
 
+  choice = short_cut(choice)
+
   computer_choice = VALID_CHOICES.sample
 
   prompt("You choose #{choice}, Computer choose #{computer_choice}")
 
   display_results(choice, computer_choice)
+  keep_score(choice, computer_choice)
 
   prompt("Do you want to play again?")
   answer = Kernel.gets().chomp()
